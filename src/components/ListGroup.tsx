f063cf642b9a8d2bb2c123915d83ed66678b6b53
elmastahsin <tahsinelmass@gmail.com>
2024-05-16 10:03:39 +0300

    managing state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,21 @@
-import {Fragment} from "react";
-import {MouseEvent} from "react";
-
+// import {Fragment} from "react";
+// import {MouseEvent} from "react";
+import {useState} from "react";
 function ListGroup() {
-    let items = ["New York", "San Fransisco", "Los Angeles", "Chicago"];
+    let items = ["New York", "San Fransisco", "Los Angeles", "Chicago", "Houston", "Paris", "London", "Berlin", "Tokyo", "Beijing"];
     // items = [];
     //Event handler
-    const handleClick = (event: MouseEvent) => console.log(event);
+    // const handleClick = (event: MouseEvent) => console.log(event);
 
     // const getMessages = () => {
     //     return items.length === 0 ? <p>No items to display</p> : null;
     // }
-let selectedIndex = 0
+    // let selectedIndex = 0;
+
+    //Hook
+    const [selectedIndex,setSelectedIndex]  = useState(-1);
+
+
     return (<>
         <h1>List Group</h1>
         {/*{getMessages()}*/}
@@ -19,10 +24,12 @@ let selectedIndex = 0
 
             {items.map((item, index) =>
                 <li className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
-                                            key={item}
-                                            onClick={handleClick}
-            >
-                {item}</li>)}
+                    key={item}
+                    onClick={() => {
+                        setSelectedIndex(index),console.log(item,index)
+                    }}
+                >
+                    {item}</li>)}
 
         </ul>
     </>);
