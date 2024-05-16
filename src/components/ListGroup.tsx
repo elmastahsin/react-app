07f72dac3dd17eeeaab2b01c7d94013ded765e7e
elmastahsin <tahsinelmass@gmail.com>
2024-05-16 09:48:45 +0300

    first react app

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,27 @@
+import {Fragment} from "react";
+import {MouseEvent} from "react";
+
 function ListGroup() {
-    let items = ["New York","San Fransisco","Los Angeles","Chicago"];
-    items= [];
+    let items = ["New York", "San Fransisco", "Los Angeles", "Chicago"];
+    // items = [];
+    //Event handler
+    const handleClick = (event: MouseEvent) => console.log(event);
+
+    // const getMessages = () => {
+    //     return items.length === 0 ? <p>No items to display</p> : null;
+    // }
 
     return (<>
         <h1>List Group</h1>
-        {items.length === 0 ? <p>No items to display</p>:null}
+        {/*{getMessages()}*/}
+        {items.length === 0 && <p>No items to display</p>}
         <ul className="list-group">
 
-            {items.map(item => <li key={item}>{item}</li>)}
+            {items.map((item, index) => <li className="list-group-item"
+                                            key={item}
+                                            onClick={handleClick}
+            >
+                {item}</li>)}
 
         </ul>
     </>);
