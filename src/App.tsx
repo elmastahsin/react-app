aba11d69919f197afc681b3b05b085cc44f9421e
elmastahsin <tahsinelmass@gmail.com>
2024-05-17 10:38:21 +0300

    Sharing State between Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,18 @@
 import {useState} from "react";
-import produce from    'immer';
+import produce from 'immer';
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
-    const [bugs, setBugs] = useState([
-        {
-            id: 1,
-            description: 'Bug 1',
-            fixed:false
-        },
-        {
-            id: 2,
-            description: 'Bug 2',
-            fixed:false
-        },
-        {
-            id: 3,
-            description: 'Bug 3',
-            fixed:false
-        }
-    ]);
 
-    const handleClick = () => {
-       // setBugs(bugs.map(bug => bug.id==1 ? {...bug, fixed:true} : bug ));
-        setBugs(produce( draft => {
-           const bug= draft.find(bug => bug.id===1);
-            if (bug) {
-                bug.fixed = true;
-            }
-        }));
-    };
+    const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
 
 
     return (
         <div>
-            {bugs.map(bug => <p key={bug.id} >{bug.description} {bug.fixed ? 'Fixed' : 'New'} </p>)}
-            {bugs.map(bug => <p key={bug.id} >{bug.description} {bug.fixed ? 'Fixed' : 'New'} </p>)}
-            {bugs.map(bug   => <p key={bug.id} >{bug.description} {bug.fixed ? 'Fixed' : 'New'} </p>)}
-            <button  onClick={handleClick}>Click Me</button>
 
+            <NavBar cartItemsCount={cartItems.length}/>
+            <Cart cartItems={cartItems} onClear={()=>setCartItems([])} />
         </div>);
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+interface Props{
+    cartItemsCount: number;
+
+}
+const NavBar = ({cartItemsCount}: Props) => {
+    return (
+        <div>NavBar : {cartItemsCount}</div>
+    )
+}
+
+export default NavBar
