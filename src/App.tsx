259d92d95fe218746f32da625e10376dbc7727b5
elmastahsin <tahsinelmass@gmail.com>
2024-05-17 09:41:10 +0300

    Simplifying Update Logic with Immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import {useState} from "react";
+import produce from    'immer';
 
 function App() {
-    const [tags, setTags] = useState(['happy', 'cheerful']);
+    const [bugs, setBugs] = useState([
+        {
+            id: 1,
+            description: 'Bug 1',
+            fixed:false
+        },
+        {
+            id: 2,
+            description: 'Bug 2',
+            fixed:false
+        },
+        {
+            id: 3,
+            description: 'Bug 3',
+            fixed:false
+        }
+    ]);
 
     const handleClick = () => {
-        //add
-       setTags([...tags,'exciting','fun']);
-
-       //remove
-        setTags(tags.filter(tag => tag !== 'cheerful'));
-
-        //update
-        setTags(tags.map(tag => tag === 'happy' ? 'joyful' : tag));
+       // setBugs(bugs.map(bug => bug.id==1 ? {...bug, fixed:true} : bug ));
+        setBugs(produce( draft => {
+           const bug= draft.find(bug => bug.id===1);
+            if (bug) {
+                bug.fixed = true;
+            }
+        }));
     };
 
 
